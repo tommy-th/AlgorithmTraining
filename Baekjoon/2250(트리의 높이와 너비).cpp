bf1bbd8fd18f415448c27eeccff70912ecee5a72
ang J <jang@jangui-macbook-pro.local>
2016-04-13 13:41:19 +0900

    Modify problem 2250.

--- Baekjoon/2250(트리의 높이와 너비).cpp
+++ Baekjoon/2250(트리의 높이와 너비).cpp
@@ -12,11 +12,9 @@
 using namespace std;
 
 int N;
-int child[10005][2], parent[10005];
+int child[10005][2], parent[10005], info[10005][2];
 int root, point;
-int max_width, max_level;
-
-vector<pair<int, int>> info;
+int pwidth, plevel, leaf;
 
 void set_root(){
     int cur = root;
@@ -27,38 +25,26 @@ void set_root(){
     root = cur;
 }
 
-void push_node(int n, int level){
-    if(child[n][0]) push_node(child[n][0], level+1);
-    info.push_back({level, ++point});
-    if(child[n][1]) push_node(child[n][1], level+1);
+void set_width(int y, int x){
+    if(info[y][0] == 0 || x < info[y][0]) info[y][0] = x;
+    if(info[y][1] == 0 || x > info[y][1]) info[y][1] = x;
+}
+
+void inorder(int n, int level){
+    if(child[n][0]) inorder(child[n][0], level+1);
+    set_width(level, ++point);
+    leaf = max(leaf, level);
+    if(child[n][1]) inorder(child[n][1], level+1);
 }
 
 void set_result() {
-    int level = 1, left, right;
     
-    left = right = info[0].second;
-    
-    for(int i=1; i<info.size(); i++){
-        
-        if(info[i].first != level){
-            int temp = right - left + 1;
-            
-            if(max_width < temp) {
-                max_width = temp;
-                max_level = level;
-            }
-            level = info[i].first;
-            left = info[i].second;
+    for(int i=1; i<=leaf; i++){
+        int width = info[i][1] - info[i][0] + 1;
+        if(width > pwidth){
+            plevel = i;
+            pwidth = width;
         }
-        
-        right = info[i].second;
-    }
-    
-    int temp = right - left + 1;
-    
-    if(max_width < temp) {
-        max_width = temp;
-        max_level = level;
     }
 }
 
@@ -85,15 +71,12 @@ int main() {
     }
     
     set_root();
-    push_node(root, 1);
     
-    sort(info.begin(), info.end(), [](pair<int, int> a, pair<int, int> b){
-        return a.first < b.first || (a.first==b.first && a.second<b.second);
-    });
+    inorder(root, 1);
     
     set_result();
     
-    cout<<max_level<<" "<<max_width<<endl;
+    cout<<plevel<<" "<<pwidth<<endl;
 
     return 0;
 }
